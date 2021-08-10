adfbc8f3daebbc8192849c52329af08240a15c4d
Grzesiek a <grzesiek7393223@o2.pl>
2021-08-10 19:03:37 +0200

    Registery

--- PomagajLokalnie/Pages/Account/Registery.cshtml.cs
+++ PomagajLokalnie/Pages/Account/Registery.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -14,12 +15,14 @@ namespace PomagajLokalnie.Pages.Account
     {
         private readonly PomagajLokalnieContext _context;
         private User _user;
-        public Roles rolesEnum;
+        
+        [BindProperty]
+        public InputModel Input { get; set; }
 
         public RegisteryModel(PomagajLokalnieContext context)
         {
             _context = context;
-           
+            Input = new InputModel();
         }
 
         public IActionResult OnGet()
@@ -27,7 +30,6 @@ namespace PomagajLokalnie.Pages.Account
             return Page();
         }
 
-        [BindProperty]
         public User User { get => _user; set => _user = value; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
@@ -37,11 +39,27 @@ namespace PomagajLokalnie.Pages.Account
             {
                 return Page();
             }
+            var User = new User() 
+            {
+                Login = Input.Login,
+                Password = Input.Password,
+                Roles = Input.Roles
+            };
+
 
             _context.Users.Add(User);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }
+        public class InputModel
+        {
+            [Required] [Display(Name = "Type")] public string Roles { get; set; }
+
+            [Required] [Display(Name = "Login")] public string Login { get; set; }
+
+            [Required] [Display(Name = "Password")] public string Password { get; set; }
+
         }
     }
 }
